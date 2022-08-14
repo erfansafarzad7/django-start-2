a9725f4141ba2fa1656bea7c33defcf6602d22c7
erfnasafazad7 <erfnasafazad7@gmail.com>
2022-08-14 20:39:03 +0430

    shell-custom-objects

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,16 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
 
+
+# class PostManager(models.Manager):
+#     def year_filter(self, year):
+#         return self.filter(publish__year= year)
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset().filter(status='published')
+
+
 class Post(models.Model):
     STATUS_CHOICES = (
         ('drafed', 'Draft'),
@@ -15,9 +25,14 @@ class Post(models.Model):
     create = models.DateTimeField(auto_now_add=True)
     update = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+    # objects=PostManager()
+    objects = models.Manager()
+    published = PublishedManager()
+
 
     class Meta:
         ordering = ('-publish',)
+
 
     def __str__(self):
         return self.title
